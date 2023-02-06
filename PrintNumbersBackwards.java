2088d71c8e9c62ad5673c9f95e48b5ffa9b8afaf
divitha-20 <110101898+divitha-20@users.noreply.github.com>
2023-02-06 20:15:06 +0530

    Add files via upload

---
+++ PrintNumbersBackwards.java
@@ -0,0 +1,15 @@
+//Java Program: PrintNumbersBackwards.java
+
+public class PrintNumbersBackwards {
+
+    public static void main(String args[]) {
+
+        int number = 100;
+        System.out.println("The numbers from 1 to 100 in reverse order : ");
+        for (int i = number; i >= 1; i--) {
+
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ PrintNumbersOneToHundred.java
@@ -0,0 +1,10 @@
+//Java Program To Print Numbers from One to Hundred
+
+public class PrintNumbersOneToHundred {
+    public static void main(String args[]) {
+        System.out.println("Numbers from One to 100:");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + ",");
+        }
+    }
+}
